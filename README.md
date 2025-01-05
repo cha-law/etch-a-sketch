3eb722c52ea14a834709427c1204d06d1eeb85e9
cha-law <char_law@outlook.com>
2025-01-05 14:00:10 +0000

    Added ability to draw and extra settings on the side

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # etch-a-sketch
+
+Etch a sketch by cha-law.
+
+Use the settings on the side to change the size of the grid or colour of the pen.
--- script.js
+++ script.js
@@ -1,5 +1,9 @@
+/* FUNCTIONS */
+
 function createSketchpad(numSquares) {
-    const sketchpadDiv = document.querySelector(".sketch-pad");
+    const sketchpadDiv = document.createElement("div");
+    sketchpadDiv.classList.add("sketch-pad");
+    document.querySelector(".sketch-pad-container").appendChild(sketchpadDiv);
     const amount = Math.pow(numSquares, 2);
     const width = 384 / numSquares;
 
@@ -7,8 +11,47 @@ function createSketchpad(numSquares) {
         let newPixel = document.createElement("div");
         newPixel.style.setProperty("width", `${width}px`);
         newPixel.classList.add("new-box");
+        newPixel.addEventListener("mouseover", draw);
         sketchpadDiv.appendChild(newPixel);
     }
 }
 
-createSketchpad(16);
+function changeSize(event) {
+    let container = document.querySelector(".sketch-pad-container");
+    let sketchpadDiv = document.querySelector(".sketch-pad");
+    let sizeDisplay = document.querySelector("#size-display");
+    let newSize = event.target.value;
+    sizeDisplay.textContent = (newSize + " x " + newSize);
+    container.removeChild(sketchpadDiv);
+    createSketchpad(newSize);
+}
+
+function changeColour(event) {
+    let colourDisplay = document.querySelector("#colour-display");
+    let newColour = event.target.value;
+    colourDisplay.textContent = newColour;
+    return newColour;
+}
+
+function draw(event) {
+    let pixel = event.target;
+    let newColour = document.querySelector("#colour-picker").value;
+    pixel.style.backgroundColor = newColour;
+}
+
+/* PROGRAM */
+
+/* Base Settings */
+let baseSize = 16;
+let colour = "#000000";
+
+
+/* Elements */
+let sizePicker = document.querySelector("#size-picker");
+let colourPicker = document.querySelector("#colour-picker");
+
+
+sizePicker.addEventListener("change", changeSize);
+colour = colourPicker.addEventListener("change", changeColour);
+
+createSketchpad(baseSize);
